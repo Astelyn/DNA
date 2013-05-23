16f15acdb99fab849dbd49167e63cf5baa30a902
Astelyn <allie@fcraft.net>
2013-05-23 09:04:57 -0400

    Added DNAApp

---
+++ src/dna/framework/dnaapp.d
@@ -0,0 +1,44 @@
+/* DNAApp - the outermost class for a DNA application. Contains the game
+   instance and takes care of initialization and the main update/render loop.
+*/
+module dna.framework.dnaapp;
+
+import std.datetime;
+
+class DNAApp {
+private:
+    bool closing;
+    double totalElapsed;
+    double lastDelta;
+
+public:
+    Game game;
+    int targetFPS;
+
+    this(Game g)
+    {
+        game = g;
+        game.load();
+
+        totalElapsed = 0.0;
+        lastDelta = 0.0;
+    }
+
+    //TODO: Add fixed timestep
+    void run()
+    {
+        while (!closing) {
+            auto start = Clock.currTime().stdTime();
+
+            game.update(lastDelta);
+            game.draw(/*graphics*/);
+
+            auto end = Clock.currTime().stdTime();
+            lastDelta = (end - start) / 10000.0;
+            totalElapsed += lastDelta;
+        }
+
+        game.quit();
+    }
+}
+
