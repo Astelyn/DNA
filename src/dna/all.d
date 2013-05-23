63a23a929e938a79cab7fbd36d6d4b654c1c249e
Astelyn <allie@fcraft.net>
2013-05-23 11:09:23 -0400

    Re-organization

---
+++ src/dna/all.d
@@ -0,0 +1,5 @@
+module dna.all;
+
+public import dna.dnaapp;
+public import dna.game;
+
---
+++ src/dna/game.d
@@ -0,0 +1,11 @@
+/* Game - the base interface for any DNA game */
+module dna.game;
+
+interface Game {
+public:
+    void load();
+    void update(double dt);
+    void draw(/*Graphics g*/);
+    void quit();
+}
+
