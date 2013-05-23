4424aebce3fe6df15d01ddd7a6071a07a013e687
Astelyn <allie@fcraft.net>
2013-05-23 10:09:23 -0400

    Added example game

---
+++ examplegame.d
@@ -0,0 +1,30 @@
+module mygame;
+
+import dna.framework;
+
+class MyGame : Game {
+public:
+    void load()
+    {
+        //Initialization code
+    }
+
+    void update(double dt)
+    {
+        //Game update logic
+    }
+
+    void draw(/*Graphics g*/)
+    {
+        //Rendering code
+    }
+}
+
+void main()
+{
+    auto app = new DNAApp(new MyGame());
+    app.targetFPS = 60;
+    //Other configuration stuff here
+    app.run();
+}
+
